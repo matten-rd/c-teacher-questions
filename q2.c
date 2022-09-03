5e3bb275489beded32d6e072ffdccfa11030e21a
matten-rd <strandfilip21@gmail.com>
2022-09-03 12:35:58 +0100

    Finished question 2

--- q2.c
+++ q2.c
@@ -2,18 +2,24 @@
 
 int main(int argc, char *argv[])
 {
-    int areas[] = { 10, 12, 13, 14, 20 };
+    char x = 'X';
     char full_name[] = {
         'Z', 'e', 'd',
         ' ', 'A', '.', ' ',
         'S', 'h', 'a', 'w'
     };
+    int areas[] = { 10, 12, 13, 14, 20 };
     char name[] = "Zed";
+
+    full_name[11] = 'A';
+    full_name[12] = 'B';
+    
+    printf("The character X is: %c\n", x);
 
     printf("The size of an int: %ld\n", sizeof(int));
     printf("The size of areas (int[]): %ld\n", sizeof(areas));
     printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
-    printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
+    printf("The first area is %d, the 2nd %d.\n", areas[10], areas[1]);
 
     printf("The size of a char: %ld\n", sizeof(char));
     printf("The size of name (char[]): %ld\n", sizeof(name));
