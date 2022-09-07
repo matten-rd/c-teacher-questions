6010db6aa670cfd789467694bc61ad334378fc83
matten-rd <strandfilip21@gmail.com>
2022-09-07 20:59:39 +0100

    First draft of question 16, the summing works as intended however it might be possible to make the code cleaner

---
+++ q16.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+typedef struct Time {
+    int hours;
+    int minutes;
+    int seconds;
+} time;
+
+time createTime(int h, int m, int s) 
+{
+    time time;
+    time.hours = h;
+    time.minutes = m;
+    time.seconds = s;
+    return time;
+}
+
+time sumTimes(time t1, time t2) 
+{
+    int sumSeconds = t1.seconds + t2.seconds;
+    int seconds = sumSeconds % 60;
+    int extraMin = (sumSeconds - seconds) / 60;
+
+    int sumMinutes = t1.minutes + t2.minutes + extraMin;
+    int minutes = sumMinutes % 60;
+    int extraHour = (sumMinutes - minutes) / 60;
+
+    int hours = t1.hours + t2.hours + extraHour;
+
+    return createTime(hours, minutes, seconds);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    time t1, t2, tSum;
+
+    t1 = createTime(12, 54, 59);
+    t2 = createTime(15, 16, 17);
+    tSum = sumTimes(t1, t2);
+
+    printf("Time1: %d:%d:%d\n", t1.hours, t1.minutes, t1.seconds);
+    printf("Time2: %d:%d:%d\n", t2.hours, t2.minutes, t2.seconds);
+    printf("TimeSum: %d:%d:%d\n", tSum.hours, tSum.minutes, tSum.seconds);
+
+    return 0;
+}
